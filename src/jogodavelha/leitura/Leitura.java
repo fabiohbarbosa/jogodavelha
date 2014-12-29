c6ea3970984e864c4e863dde0a3edb08b88d51a4
Fábio H. G. Barbosa <fabiohbarbosa@gmail.com>
2014-12-28 22:13:32 -0200

    Corrigindo lógica

--- src/jogodavelha/leitura/Leitura.java
+++ src/jogodavelha/leitura/Leitura.java
@@ -9,10 +9,10 @@ public class Leitura {
 		in = new Scanner(System.in);
 		System.out.printf("Informe o Jogador:  ");
 		int jogador = in.nextInt();
-		if (jogador < 1 && jogador > 2){
+		if (jogador < 1 || jogador > 2){
 			System.out.println("Jogador informado incorreto.");
 		}else {
-			System.out.println("Jogador informado é: " + jogador);
+			System.out.println("Jogador informado Ã©: " + jogador);
 		}
 	}
 }
