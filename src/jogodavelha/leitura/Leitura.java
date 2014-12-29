fc3a79c3a28b5a14b6700db3bbe6b5bff6f6f72d
Thiago Barbosa <t-a-barbosa@hotmail.com>
2014-12-28 23:18:24 -0200

    Adicionado jogador, coluna e linha

--- src/jogodavelha/leitura/Leitura.java
+++ src/jogodavelha/leitura/Leitura.java
@@ -4,15 +4,40 @@ import java.util.Scanner;
 
 public class Leitura {
 	private Scanner in;
-
 	public void lerDados() throws Exception {
 		in = new Scanner(System.in);
 		System.out.printf("Informe o Jogador:  ");
 		int jogador = in.nextInt();
-		if (jogador < 1 || jogador > 2){
-			System.out.println("Jogador informado incorreto.");
+		if (jogador == 1 || jogador == 2){
+			System.out.println("Jogador informado corretamente: " + jogador);
 		}else {
-			throw new Exception();
+			throw new Exception("Por favor informar numero do jogador de 1 a 2.");
 		}
+		System.out.printf("Informe a jogada: ");
+		String jogada = in.next();
+		if (jogada.equals("x") || jogada.equals("o") ){
+			System.out.println("jogada informada corretamente " + jogada);
+		}else {
+			throw new Exception ("Por favor informar uma jogada corretamente. ");
+		}
+		System.out.printf("por favor informar a Coluna: ");
+		int coluna = in.nextInt();
+		if (coluna >= 0 || coluna < 3){
+			System.out.println("Coluna aceita: " + coluna);
+		}else {
+			throw new Exception ("Por favor informar coluna entre 0 a 2.");
+		}
+		System.out.printf("Por favor informar a Linha: ");
+		int linha = in.nextInt();
+		if (linha >= 0 || linha < 3){
+			System.out.println("Linha aceita " + linha);
+		}else {
+			throw new Exception ("Por favor informar coluna entre 0 a 2");
+		}
+		iniciarJogada(jogador,jogada,coluna,linha);
+			
+	}
+	private void iniciarJogada(int jogador, String jogada, int coluna, int linha) {
+		
 	}
 }
